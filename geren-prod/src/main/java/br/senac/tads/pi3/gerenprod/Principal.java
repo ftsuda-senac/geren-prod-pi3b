774eacdcec7bb1a123834da3b80c081347c5adf7
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-02-28 21:57:41 -0300

    Alteração da lógica 2

--- geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/Principal.java
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/Principal.java
@@ -15,6 +15,9 @@ public class Principal {
         System.out.println("Teste Maven");
         int i = 0;
         System.out.println("Valor i++ = " + i++);
+        for (int y = 0; y < 10; y++) {
+            System.out.println("Valor y: " + y);
+        }
     }
 
 }
